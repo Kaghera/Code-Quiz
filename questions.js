02b3913b000883b7a05ed9fce036b342ea8fe38e
Kashyap Aghera <kaagh.10@gmail.com>
2020-06-18 16:10:42 -0400

    Added project files.
    Added index.html, style.css and questions.js files.

---
+++ questions.js
@@ -0,0 +1,26 @@
+var startbutton = document.getElementsByClassName('start-btn');
+
+startbutton.addEventListener('click', function (){
+  console.log('You have single-clicked');
+});
+
+
+// var questions = [
+//   {
+//     title: "Commonly used data types DO NOT include:",
+//     choices: ["strings", "booleans", "alerts", "numbers"],
+//     answer: "alerts"
+//   },
+//   {
+//     title: "The condition in an if / else statement is enclosed within ____.",
+//     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+//     answer: "parentheses"
+//   },
+//   ///etc.
+// ];
+
+// document.getElementById('start-btn').onclick = function changeContent() {
+//   document.getElementById('start-btn')
+// }
+
+// console.log(questions[0].title);
