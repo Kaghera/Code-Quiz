4165865d80823a6f8e0cc6cbfa493e5cc9a64a76
Kashyap Aghera <kaagh.10@gmail.com>
2020-06-18 18:24:42 -0400

    file modified.
    New divs and labels added in body section.

--- questions.js
+++ questions.js
@@ -1,26 +1,71 @@
-var startbutton = document.getElementsByClassName('start-btn');
-
-startbutton.addEventListener('click', function (){
-  console.log('You have single-clicked');
-});
-
-
-// var questions = [
-//   {
-//     title: "Commonly used data types DO NOT include:",
-//     choices: ["strings", "booleans", "alerts", "numbers"],
-//     answer: "alerts"
-//   },
-//   {
-//     title: "The condition in an if / else statement is enclosed within ____.",
-//     choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-//     answer: "parentheses"
-//   },
-//   ///etc.
-// ];
-
-// document.getElementById('start-btn').onclick = function changeContent() {
-//   document.getElementById('start-btn')
-// }
-
-// console.log(questions[0].title);
+var questions = [{
+	"question": "Commonly used data types DO NOT include:",
+	"option1": "strings",
+	"option2": "booleans",
+	"option3": "alerts",
+	"option4": "numbers",
+	"answer": "alerts"
+}, {
+	"question": "The condition in an if / else statement is enclosed within ____.",
+	"option1": "quotes",
+	"option2": "curly brackets",
+	"option3": "parentheses",
+	"option4": "square brackets",
+	"answer": "parentheses"
+}, {
+	"question": "Arrays in JavaScript can be used to store _____.",
+	"option1": "numbers and strings",
+	"option2": "other arrays",
+	"option3": "booleans",
+	"option4": "all of the above",
+	"answer": "all of the above"
+}, {
+	"question": "String values must be enclosed within _____ when being assigned to variables.",
+	"option1": "commas",
+	"option2": "curly brackets",
+	"option3": "quotes",
+	"option4": "parentheses",
+	"answer": "quotes"
+}, {
+	"question": "A very useful tool used during development and debugging for printing content to the debugger is: ",
+	"option1": "JavaScript",
+	"option2": "terminal/bash",
+	"option3": "for loops",
+	"option4": "console.log",
+	"answer": "console.log"
+}, {
+	"question": "Inside which HTML element do we put the JavaScript?",
+	"option1": "<script>",
+	"option2": "<js>",
+	"option3": "<scripting>",
+	"option4": "<javascript>",
+	"answer": "<script>"
+}, {
+	"question": "Where is the correct place to insert a JavaScript?",
+	"option1": "The <head> section",
+	"option2": "The <body> section",
+	"option3": "Both <head> and <body> section",
+	"option4": "None of the above",
+	"answer": "Both <head> and <body> section"
+}, {
+	"question": "How to write an IF statement in JavaScript?",
+	"option1": "if (i == 5)",
+	"option2": "if i == 5 then",
+	"option3": "if i = 5",
+	"option4": "if i = 5 then",
+	"answer": "if (i == 5)"
+}, {
+	"question": "How does a FOR loop start?",
+	"option1": "for (i = 0; i <= 5; i++)",
+	"option2": "for i = 1 to 5",
+	"option3": "for (i = 0; i <= 5)",
+	"option4": "for (i <= 5; i++)",
+	"answer": "for (i = 0; i <= 5; i++)"
+}, {
+	"question": "How do you round the number 7.25, to the nearest integer?",
+	"option1": "Math.round(7.25)",
+	"option2": "round(7.25",
+	"option3": "rnd(7.25)",
+	"option4": "Math.rnd(7.25)",
+	"answer": "Math.round(7.25)"
+}];
