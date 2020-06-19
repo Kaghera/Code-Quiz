df795ce9bc4e6130fa7d5930ce0ba78dd1ac8f92
Kashyap Aghera <kaagh.10@gmail.com>
2020-06-18 23:28:26 -0400

    Cleared Errors.

--- questions.js
+++ questions.js
@@ -3,69 +3,69 @@ var questions = [{
 	"option1": "strings",
 	"option2": "booleans",
 	"option3": "alerts",
-	"option4": "numbers",
-	"answer": "alerts"
+	"option4": "numbers", 
+	"answer": "3"
 }, {
 	"question": "The condition in an if / else statement is enclosed within ____.",
 	"option1": "quotes",
 	"option2": "curly brackets",
 	"option3": "parentheses",
 	"option4": "square brackets",
-	"answer": "parentheses"
+	"answer": "3"
 }, {
 	"question": "Arrays in JavaScript can be used to store _____.",
 	"option1": "numbers and strings",
 	"option2": "other arrays",
 	"option3": "booleans",
 	"option4": "all of the above",
-	"answer": "all of the above"
+	"answer": "4"
 }, {
 	"question": "String values must be enclosed within _____ when being assigned to variables.",
 	"option1": "commas",
 	"option2": "curly brackets",
 	"option3": "quotes",
 	"option4": "parentheses",
-	"answer": "quotes"
+	"answer": "3"
 }, {
 	"question": "A very useful tool used during development and debugging for printing content to the debugger is: ",
 	"option1": "JavaScript",
 	"option2": "terminal/bash",
 	"option3": "for loops",
 	"option4": "console.log",
-	"answer": "console.log"
+	"answer": "4"
 }, {
 	"question": "Inside which HTML element do we put the JavaScript?",
 	"option1": "<script>",
 	"option2": "<js>",
 	"option3": "<scripting>",
 	"option4": "<javascript>",
-	"answer": "<script>"
+	"answer": "1"
 }, {
 	"question": "Where is the correct place to insert a JavaScript?",
 	"option1": "The <head> section",
 	"option2": "The <body> section",
 	"option3": "Both <head> and <body> section",
 	"option4": "None of the above",
-	"answer": "Both <head> and <body> section"
+	"answer": "3"
 }, {
 	"question": "How to write an IF statement in JavaScript?",
 	"option1": "if (i == 5)",
 	"option2": "if i == 5 then",
 	"option3": "if i = 5",
 	"option4": "if i = 5 then",
-	"answer": "if (i == 5)"
+	"answer": "1"
 }, {
 	"question": "How does a FOR loop start?",
 	"option1": "for (i = 0; i <= 5; i++)",
 	"option2": "for i = 1 to 5",
 	"option3": "for (i = 0; i <= 5)",
 	"option4": "for (i <= 5; i++)",
-	"answer": "for (i = 0; i <= 5; i++)"
+	"answer": "1"
 }, {
 	"question": "How do you round the number 7.25, to the nearest integer?",
 	"option1": "Math.round(7.25)",
 	"option2": "round(7.25",
 	"option3": "rnd(7.25)",
 	"option4": "Math.rnd(7.25)",
-	"answer": "Math.round(7.25)"
+	"answer": "1"
 }];
